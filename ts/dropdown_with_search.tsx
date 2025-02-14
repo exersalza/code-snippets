8c7588bced6b76784d7452cbcc7b4be6f16b09dd
exersalza "Julian J. <squidodle44@gmail.com>
2025-02-14 13:15:04 +0100

    not 100% twcss, but should be good enough

--- ts/dropdown_with_search.tsx
+++ ts/dropdown_with_search.tsx
@@ -3,6 +3,7 @@ import { Icons } from "./Icons";
 
 interface Props {
   values: string[],
+  disabled?: boolean,
   callback: (value: string) => void;
 }
 
@@ -24,13 +25,17 @@ export function Dropdown(props: Props) {
   let itemsRef = useRef<HTMLDivElement>(null);
   let bodyRef = useRef<HTMLDivElement>(null);
 
+  useEffect(() => {
+    setStates((prev) => ({ ...prev, values: props.values }));
+  }, [props.values])
+
 
   const inputHandler = (e: InputEvent) => {
     const value = (e.target as HTMLInputElement).value;
     const reg = new RegExp(`.*(${value}).*`, "gi");
 
     setStates((prev) => ({
-      ...prev, 
+      ...prev,
       values: props.values.filter((i) => {
         reg.lastIndex = 0; return reg.test(i)
       }),
@@ -38,6 +43,7 @@ export function Dropdown(props: Props) {
     }))
   }
 
+  // make the dropdown close when you click outside of it
   const outsideClickHandler = (e: MouseEvent) => {
     const target = e.target as Node;
 
@@ -48,7 +54,7 @@ export function Dropdown(props: Props) {
 
   function cb(value: string) {
     props.callback(value);
-    setStates((prev) => ({...prev, open: false, lastSelectedValue: value}));
+    setStates((prev) => ({ ...prev, open: false, lastSelectedValue: value }));
   }
 
   useEffect(() => {
@@ -61,14 +67,14 @@ export function Dropdown(props: Props) {
 
 
   return (
-    <div className={"w-46 max-h-60 bg-gray-100 rounded-lg select-none transition-all dropdown-parent"} ref={bodyRef}>
+    <div className={`w-46 max-h-60 z-100 bg-gray-100 relative ${states.open ? "rounded-t-lg" : "rounded-lg" } select-none transition-all dropdown-parent`} ref={bodyRef}>
       <div className={"flex place-items-center "} onClick={() => {
         setStates((prev) => ({ ...prev, open: true }))
       }}>
         <input
           onInput={inputHandler}
           className={"px-1 py-1 outline-none w-8/9"}
-          placeholder={"Search..."} 
+          placeholder={"Search..."}
           value={states.lastSelectedValue}
         />
         <p onClick={(e) => {
@@ -79,9 +85,14 @@ export function Dropdown(props: Props) {
           {Icons.chevron_down}
         </p>
       </div>
-      <div className={`px-1 max-h-49 flex flex-col transition-all duration-100 overflow-auto good-scrollbar rounded-lg scrollbar scrollbar-thumb-zinc-400`}
+      <div className={`px-1 max-h-49 w-full absolute flex flex-col transition-all overflow-auto good-scrollbar rounded-b-lg scrollbar scrollbar-thumb-zinc-400 bg-gray-100 duration-100`}
         ref={itemsRef}
-        style={{ height: `calc(${states.open ? props.values.length : 0} * 1.5rem)` }}>
+        style={{
+          height: `calc(${states.open ? Math.min(props.values.length, 9) : 0} * 1.5rem)`,
+          top: '100%',
+          visibility: states.open ? 'visible' : 'hidden',
+          opacity: states.open ? 1 : 0,
+        }}>
         {states.values.map((value) => {
           return <p
             onClick={() => cb(value)}
