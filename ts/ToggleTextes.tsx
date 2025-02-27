674829b0c7cde23cde765b439f1b9f80cef00172
exersalza "Julian J. <squidodle44@gmail.com>
2025-02-27 11:03:12 +0100

    Update ToggleTextes.tsx

--- ts/ToggleTextes.tsx
+++ ts/ToggleTextes.tsx
@@ -6,11 +6,10 @@ interface Props {
 }
 
 export function ChangeButton(props: Props) {
-
   return (
     <div className={`flex gap-2 relative ${props.className ?? ""}`}>
       {props.options.map((val) => (
-        <button onClick={()=>{props.setActive(val)}} className={`${props?.active === val ? "text-gray-800" : "text-gray-400"} ${props.active === undefined ? "first:text-gray-800" : ""} transition-color cursor-pointer`}>{val}</button>
+        <button onClick={()=>{props.setActive(val)}} className={`${props?.active === val ? "text-gray-800" : "text-gray-800/60"} font-semibold ${props.active === undefined ? "first:text-gray-800" : ""} transition-color cursor-pointer`}>{val}</button>
       ))}
     </div>
   )
